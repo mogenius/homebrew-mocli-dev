d65fe756d8edfeda45f9889f1ce497bf7fba2f60
mocli <mocli@mogenius.com>
2023-11-27 09:52:32 +0000

    v1.6.17-dev

--- mocli-dev.rb
+++ mocli-dev.rb
@@ -2,7 +2,7 @@ class MocliDev < Formula
   desc "View your mogenius account in style from your CLI environment! [dev]"
   homepage "https://www.mogenius.com"
   
-  version "1.6.16-dev"
+  version "1.6.17-dev"
   license "MIT"
 
   test do
@@ -10,29 +10,29 @@ class MocliDev < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-darwin-arm64.tar.gz"
-      sha256 "4987289eaae35dd9ee4ffa3bd03851a1af6d487d263b7faa48061a70a4dd8bce"
+      url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-darwin-arm64.tar.gz"
+      sha256 "b151eaf65e1047aefa823343b25d702eaf6c6fb14b0b9bbd2e69723edd274883"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-darwin-amd64.tar.gz"
-      sha256 "e6312e8f793aff815c762df3fe0a414fbe8937557bfbc707dee576faade53277"
+      url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-darwin-amd64.tar.gz"
+      sha256 "99ec8bd1755a4af1591fce0e96cc53b3db02d9e015f2bb85d4ee576cb0a94c1d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-linux-amd64.tar.gz"
+        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-linux-amd64.tar.gz"
         sha256 ""
       else
-        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-linux-386.tar.gz"
+        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-linux-386.tar.gz"
         sha256 ""
       end
     elif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-linux-arm64.tar.gz"
+        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-linux-arm64.tar.gz"
         sha256 ""
       else
-        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.16-dev/mocli-dev-v1.6.16-dev-linux-arm.tar.gz"
+        url "https://github.com/mogenius/homebrew-mocli-dev/releases/download/v1.6.17-dev/mocli-dev-v1.6.17-dev-linux-arm.tar.gz"
         sha256 ""
       end
     end
@@ -42,27 +42,27 @@ class MocliDev < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       # Installation steps for macOS ARM64
-      bin.install "mocli-dev-v1.6.16-dev-darwin-arm64" => "mocli-dev"
+      bin.install "mocli-dev-v1.6.17-dev-darwin-arm64" => "mocli-dev"
     elsif Hardware::CPU.intel?
       # Installation steps for macOS AMD64
-      bin.install "mocli-dev-v1.6.16-dev-darwin-amd64" => "mocli-dev"
+      bin.install "mocli-dev-v1.6.17-dev-darwin-amd64" => "mocli-dev"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux AMD64
-        bin.install "mocli-dev-v1.6.16-dev-linux-amd64" => "mocli-dev"
+        bin.install "mocli-dev-v1.6.17-dev-linux-amd64" => "mocli-dev"
       else
         # Installation steps for Linux 386
-        bin.install "mocli-dev-v1.6.16-dev-linux-386" => "mocli-dev"
+        bin.install "mocli-dev-v1.6.17-dev-linux-386" => "mocli-dev"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         # Installation steps for Linux ARM64
-        bin.install "mocli-dev-v1.6.16-dev-linux-arm64" => "mocli-dev"
+        bin.install "mocli-dev-v1.6.17-dev-linux-arm64" => "mocli-dev"
       else
         # Installation steps for Linux ARM
-        bin.install "mocli-dev-v1.6.16-dev-linux-arm" => "mocli-dev"
+        bin.install "mocli-dev-v1.6.17-dev-linux-arm" => "mocli-dev"
       end
     end
   end
